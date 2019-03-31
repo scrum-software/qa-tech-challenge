aec6bdd72d3a24509b296a35649a3c741342132b
Boris Levanov <boris.levanov@ef.uk.com>
2019-03-31 12:25:27 +0100

    challenge has been successfully passed, will do some additional work

--- src/test/e2e/tests/test_arrays_challenge.py
+++ src/test/e2e/tests/test_arrays_challenge.py
@@ -8,10 +8,12 @@ def test_arrays_challenge(browser):
     assert initial_page.render_challenge_button.text == 'RENDER THE CHALLENGE'
 
     initial_page.go_to_challenge()
-    assert initial_page.submit_button.text == 'SUBMIT ANSWERS'
 
     challenge_one = ChallengeOne(browser)
     cell_values_object = TableToDictionary().convert_table_to_dict(challenge_one.table.table_rows)
 
     completed_answers = ListParser().derive_answers(cell_values_object)
-    challenge_one.populate_answers_and_submit(completed_answers)
+    # Temporarily disabled the below to ensure the slack isn't spammed
+    # challenge_one.populate_answers_and_submit(completed_answers)
+
+    pass
